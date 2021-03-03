2f88093d64f47cc153842c97142956ad31c63235
Matteo Nizzo <nizzo.matte@gmail.com>
2021-03-03 22:30:39 +0100

    starting working on map

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+function pageLoaded() {
+  // enable the scrolling
+  $(".page-1").css("position", "initial");
+
+  // make the next-page button clickable
+  $(".next-page").click(() => {
+    $(window).scrollTop($(window).height());
+  });
+}
+
+// MAIN
 let animationCount = $(".animation").length;
 let animationEndedCount = 0;
 
+// disable the scrolling to let the animations play
+$(".page-1").css("position", "fixed");
 
-function noScroll() {
-  window.scrollTo(0, 0);
-}
-
-$(window).on("scroll", noScroll);
-
+// when one animation ends
 $(window).on("animationend", () => {
   animationEndedCount++;
 
-  if (animationEndedCount >= animationCount) {
-    $(window).off("scroll");
-  }
+  // when all the animations ended
+  if (animationEndedCount >= animationCount) pageLoaded();
 });
 
-
-
+// loading the map
 $(".map-container").mapael({
   map: {
     name: "european_union",
